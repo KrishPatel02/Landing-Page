98bb2314f47c488f93a94cf790ab4075e5338c3b
Krish Patel <krishpatel200508@gmail.com>
2024-05-01 20:05:56 +0530

    Create Contact Us Page

--- src/Pages/ContactUs.jsx
+++ src/Pages/ContactUs.jsx
@@ -1,12 +1,29 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react'
+import React from "react";
+import "../Page CSS/ContactUs.css";
 
-const ContactUs = () => {
+const Contact = () => {
     return (
         <>
-            <div>ContactUs</div>
+            <div className="contactUs">
+                <div className="contactUsForm">
+                    <h1>Contact Us</h1>
+                    <form>
+                        <div className="inputField">
+                            <input type="text" placeholder="Name" />
+                        </div>
+                        <div className="inputField">
+                            <input type="email" placeholder="Email" />
+                        </div>
+                        <div className="inputField">
+                            <textarea rows="5" placeholder="Message"></textarea>
+                        </div>
+                        <button type="submit">Submit</button>
+                    </form>
+                </div>
+            </div>
         </>
-    )
-}
+    );
+};
 
-export default ContactUs
+export default Contact;
