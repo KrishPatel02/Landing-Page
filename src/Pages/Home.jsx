af94d87699009087c2b93169f2148bbb978a5695
Krish Patel <krishpatel200508@gmail.com>
2024-05-01 19:23:56 +0530

    Create Home Page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,13 +1,33 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
-// import Navbar from "../Components/Navbar";
+import "../Page CSS/Home.css";
+import { GoSignIn } from "react-icons/go";
+import { IoMdPricetag } from "react-icons/io";
+import { NavLink } from "react-router-dom";
 
 const Home = () => {
     return (
-        <>
-            {/* <Navbar /> */}
-            <div className="home">Home</div>
-        </>
+        <header className="header">
+            <div className="headerLeft">
+                <h1>Transform Your Business Digitally</h1>
+                <p>
+                    Unlock the potential of your business with our transformative digital
+                    solutions. Elevate your operations, reach new heights, and stay ahead
+                    of the competition with our cutting-edge technology services.
+                </p>
+                <div className="headerLeftBtn">
+                    <NavLink to="/Pricing" className="pricingBtn headerBtn">
+                        <IoMdPricetag />
+                        Pricing
+                    </NavLink>
+                    <NavLink to="/Registration" className="signinBtn headerBtn">
+                        <GoSignIn />
+                        Sign In
+                    </NavLink>
+                </div>
+            </div>
+            <div></div>
+        </header>
     );
 };
 
