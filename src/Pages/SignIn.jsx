eddf1eb89ba0d270c9a7ff200d819657a3be534a
Krish Patel <krishpatel200508@gmail.com>
2024-05-02 14:51:14 +0530

    Create SignIn Page

--- src/Pages/SignIn.jsx
+++ src/Pages/SignIn.jsx
@@ -1,11 +1,38 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
+import "../Page CSS/SignIn.css";
+import { MdEmail } from "react-icons/md";
+import { TbPassword } from "react-icons/tb";
+import { PiSignInBold } from "react-icons/pi";
+import { FcGoogle } from "react-icons/fc";
 
 const SignIn = () => {
     return (
-        <>
-            <div className="SignIn">SignIn</div>
-        </>
+        <div className="signIn">
+            <div className="signInForm">
+                <h1>Sign In</h1>
+                <form>
+                    <div className="inputField">
+                        <MdEmail className="icon" />
+                        <input type="email" placeholder="Email" />
+                    </div>
+                    <div className="inputField">
+                        <TbPassword className="icon" />
+                        <input type="password" placeholder="Password" />
+                    </div>
+                    <button type="submit">
+                        <pre>Sign In </pre>
+                        <PiSignInBold />
+                    </button>
+                </form>
+                <div className="or">
+                    <span>Or</span>
+                </div>
+                <button className="googleSignIn">
+                    <pre>Sign In with </pre> <FcGoogle />
+                </button>
+            </div>
+        </div>
     );
 };
 
