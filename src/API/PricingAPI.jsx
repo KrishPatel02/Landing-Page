007d092a416331783d9e4e07797033040eae477e
Krish Patel <krishpatel200508@gmail.com>
2024-05-02 21:24:12 +0530

    Create add Pricing Page

---
+++ src/API/PricingAPI.jsx
@@ -0,0 +1,141 @@
+const Plans = [
+    {
+        name: "Basic",
+
+        price: "₹1000/month",
+
+        featureLimited: [
+            "10 GB Storage",
+
+            "50 GB Bandwidth",
+
+            "1 Domain",
+
+            "Weekly Backups",
+        ],
+
+        features: [
+            "24/7 Support",
+
+            "SSL Certificate",
+
+            "Free CDN",
+
+            "Email Accounts",
+
+            "FTP Access",
+        ],
+
+        featureExcluded: [
+            "Marketing Integrations",
+
+            "AI Copywriting Tool",
+
+            "AI Heatmaps",
+
+            "AI SEO Tools",
+
+            "0% Transaction Fees",
+
+            "Google Analytics Integration",
+
+            "Free Migration",
+
+            "99.9% Uptime",
+        ],
+    },
+
+    {
+        name: "Standard",
+
+        price: "₹1849/month",
+
+        featureLimited: [
+            "200 GB Storage",
+
+            "150 GB Bandwidth",
+
+            "10 Domain",
+
+            "Daily Backups",
+        ],
+
+        features: [
+            "24/7 Support",
+
+            "SSL Certificate",
+
+            "Free CDN",
+
+            "Email Accounts",
+
+            "FTP Access",
+
+            "Marketing Integrations",
+
+            "AI Copywriting Tool",
+        ],
+
+        featureExcluded: [
+            "AI Heatmaps",
+
+            "AI SEO Tools",
+
+            "0% Transaction Fees",
+
+            "Google Analytics Integration",
+
+            "Free Migration",
+
+            "99.9% Uptime",
+        ],
+    },
+
+    {
+        name: "Premium",
+
+        price: "₹3849/month",
+
+        featureLimited: [],
+
+        features: [
+            "1000 GB Storage",
+
+            "Unlimited Bandwidth",
+
+            "20 Domain",
+
+            "12 Hours Backups",
+
+            "24/7 Support",
+
+            "SSL Certificate",
+
+            "Free CDN",
+
+            "Email Accounts",
+
+            "FTP Access",
+
+            "Marketing Integrations",
+
+            "AI Copywriting Tool",
+
+            "AI Heatmaps",
+
+            "AI SEO Tools",
+
+            "0% Transaction Fees",
+
+            "Google Analytics Integration",
+
+            "Free Migration",
+
+            "99.9% Uptime",
+        ],
+
+        featureExcluded: [],
+    },
+];
+
+export default Plans;
--- src/Pages/Pricing.jsx
+++ src/Pages/Pricing.jsx
@@ -1,10 +1,62 @@
 // eslint-disable-next-line no-unused-vars
+
 import React from "react";
+
+import { FaCheck } from "react-icons/fa";
+
+import { ImCross } from "react-icons/im";
+
+import "../Page CSS/Pricing.css";
+
+import Plans from "../API/PricingAPI";
+
+import { IoMdPricetag } from "react-icons/io";
 
 const Pricing = () => {
     return (
         <>
-            <div>Pricing</div>
+            <h1 className="pricingTitle">
+                <IoMdPricetag />
+                Pricing
+            </h1>
+
+            <div className="pricing">
+                {Plans.map((plan, index) => (
+                    <div key={index} className="plan">
+                        {index == 1 ? <div className="popularTag">Most Popular</div> : ""}
+
+                        <h2>{plan.name}</h2>
+
+                        <div className="planPrice">{plan.price}</div>
+
+                        <ul className="planFeatureList">
+                            {plan.featureLimited.map((feature, i) => (
+                                <li key={i}>
+                                    <FaCheck className="checkOrangeIcon pricingIcon" />
+
+                                    {feature}
+                                </li>
+                            ))}
+
+                            {plan.features.map((feature, i) => (
+                                <li key={i}>
+                                    <FaCheck className="checkGreenIcon pricingIcon" /> {feature}
+                                </li>
+                            ))}
+
+                            {plan.featureExcluded.map((feature, i) => (
+                                <li key={i}>
+                                    <ImCross className="crossRedIcon pricingIcon" />
+
+                                    {feature}
+                                </li>
+                            ))}
+                        </ul>
+
+                        <button className="chooseBtn">Choose Plan</button>
+                    </div>
+                ))}
+            </div>
         </>
     );
 };
