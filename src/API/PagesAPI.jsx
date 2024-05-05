25a3e904d03e4272d446003a75785f81d85cfde3
Krish Patel <krishpatel200508@gmail.com>
2024-05-05 15:15:29 +0530

    Create Pages API

---
+++ src/API/PagesAPI.jsx
@@ -0,0 +1,29 @@
+const Pages = [
+    {
+        id: 1,
+        pageName: "Services",
+        pageLink: "/Services"
+    },
+    {
+        id: 2,
+        pageName: "Pricing",
+        pageLink: "/Pricing"
+    },
+    {
+        id: 3,
+        pageName: "FAQs",
+        pageLink: "/FAQs"
+    },
+    {
+        id: 4,
+        pageName: "ContactUs",
+        pageLink: "/ContactUs"
+    },
+    {
+        id: 5,
+        pageName: "SignIn",
+        pageLink: "/SignIn"
+    },
+];
+
+export default Pages;
